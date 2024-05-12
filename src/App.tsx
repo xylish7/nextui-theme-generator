79a1b185d2eef69163c2eed44dacd36e13358341
Filip Frincu <filip.frincu@visma.com>
2024-05-12 14:24:37 +0300

    :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <>
+      <Button color="primary">test</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </React.StrictMode>
+);
