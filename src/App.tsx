787bb93d4a9e57e267052d89dde80f5f57e02b7c
Filip Frincu <filip.frincu@visma.com>
2024-05-13 15:28:52 +0300

    :sparkles: Create ShowcaseComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@nextui-org/react";
+import { Button, Chip } from "@nextui-org/react";
+import ShowcaseComponent from "./components/showcase-component";
 
 function App() {
   return (
-    <>
-      <Button color="primary">test</Button>
-    </>
+    <main className="max-w-6xl mx-auto">
+      <div className="grid grid-cols-2 gap-4 py-8">
+        <ShowcaseComponent name="Button">
+          <Button color="default">Default</Button>
+          <Button color="primary">Primary</Button>
+          <Button color="secondary">Secondary</Button>
+          <Button color="success">Success</Button>
+          <Button color="warning">Warning</Button>
+          <Button color="danger">Danger</Button>
+        </ShowcaseComponent>
+        <ShowcaseComponent name="Chip">
+          <Chip color="default">Default</Chip>
+          <Chip color="primary">Primary</Chip>
+          <Chip color="secondary">Secondary</Chip>
+          <Chip color="success">Success</Chip>
+          <Chip color="warning">Warning</Chip>
+          <Chip color="danger">Danger</Chip>
+        </ShowcaseComponent>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/showcase-component.tsx
@@ -0,0 +1,85 @@
+import { Select, SelectItem } from "@nextui-org/react";
+import { Children, cloneElement, useState } from "react";
+
+interface ShowcaseComponentProps {
+  children: React.ReactElement[];
+  name: string;
+}
+
+export default function ShowcaseComponent({
+  children,
+  name,
+}: ShowcaseComponentProps) {
+  const [variant, setVariant] = useState<Variant>("solid");
+  const [size, setSize] = useState<Size>("md");
+
+  return (
+    <div className="p-4 border rounded-lg">
+      <span className="text-xl font-semibold">{name}</span>
+      <div className="flex gap-2 mt-4">
+        <Select
+          className="w-32"
+          defaultSelectedKeys={[variant]}
+          label="Variant"
+          labelPlacement="outside"
+          selectedKeys={[variant]}
+          size="sm"
+          onChange={(e) => setVariant((e.target.value as Variant) || "solid")}
+        >
+          {variants.map((variant) => (
+            <SelectItem key={variant.value} value={variant.value}>
+              {variant.label}
+            </SelectItem>
+          ))}
+        </Select>
+        <Select
+          className="w-32"
+          label="Size"
+          labelPlacement="outside"
+          defaultSelectedKeys={[size]}
+          selectedKeys={[size]}
+          size="sm"
+          onChange={(e) => setSize((e.target.value as Size) || "md")}
+        >
+          {sizes.map((size) => (
+            <SelectItem key={size.value} value={size.value}>
+              {size.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <div className="flex flex-wrap gap-2 mt-8">
+        {Children.map(children, (child) =>
+          cloneElement(child, { size, variant })
+        )}
+      </div>
+    </div>
+  );
+}
+
+const variants: { label: string; value: Variant }[] = [
+  { label: "Solid", value: "solid" },
+  { label: "Faded", value: "faded" },
+  { label: "Bordered", value: "bordered" },
+  { label: "Light", value: "light" },
+  { label: "Flat", value: "flat" },
+  { label: "Ghost", value: "ghost" },
+  { label: "Shadow", value: "shadow" },
+];
+
+const sizes: { label: string; value: Size }[] = [
+  { label: "Small", value: "sm" },
+  { label: "Medium", value: "md" },
+  { label: "Large", value: "lg" },
+];
+
+type Size = "sm" | "md" | "lg";
+
+type Variant =
+  | "solid"
+  | "faded"
+  | "bordered"
+  | "light"
+  | "flat"
+  | "ghost"
+  | "shadow";
