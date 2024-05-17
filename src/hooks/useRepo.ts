685b92d2d8981a2ae90280b821feb4044998dedf
Filip Frincu <filipfrincu@gmail.com>
2024-05-17 10:11:34 +0300

    :recycle: Simplify useRepo hook

--- src/hooks/useRepo.ts
+++ src/hooks/useRepo.ts
@@ -1,36 +1,16 @@
 import { useEffect, useState } from "react";
 import { getRepoStars } from "lib/github";
-import { Repo } from "shared/types";
 
 export default function useRepo(owner: string, repoName: string) {
   const [stars, setStars] = useState<number | null>(null);
-  const repoKey = `${repoName}-${owner}-repo`;
 
   useEffect(() => {
-    const stringifiedRepo = localStorage.getItem(repoKey);
-    const repo: Repo | null = stringifiedRepo
-      ? JSON.parse(stringifiedRepo)
-      : null;
-    const updatedAt = repo ? new Date(repo.updatedAt) : null;
-    const now = new Date();
-
-    if (!repo || (updatedAt && now.getTime() - updatedAt.getTime() > 3600000)) {
+    if (import.meta.env.PROD) {
       getRepoStars(owner, repoName).then((stars) => {
         setStars(stars ?? null);
-        localStorage.setItem(
-          repoKey,
-          JSON.stringify({
-            name: repo,
-            owner,
-            stars,
-            updatedAt: new Date().toISOString(),
-          })
-        );
       });
-    } else {
-      setStars(repo.stars);
     }
-  }, [owner, repoName, repoKey]);
+  }, [owner, repoName]);
 
   return stars;
 }
