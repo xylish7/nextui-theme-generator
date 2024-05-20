a2531c26c63cdb29cb142b57577b8b9e7810c3ef
Filip Frincu <filipfrincu@gmail.com>
2024-05-20 11:27:19 +0300

    :recycle: Rename files dues to case issues

--- src/components/CLayout.tsx
+++ src/components/CLayout.tsx
@@ -1,5 +1,5 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./CFooter";
+import Navbar from "./CNavbar";
 
 interface LayoutProps {
   children: React.ReactNode;
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "components/Layout";
+import { Layout } from "components/CLayout";
 import Configuration from "./Configuration";
 import Showcase from "./Showcase";
 import Info from "./Info";
